e5fec302e2ae398d4b4be2f03192b4c49a9c04b7
Classic-username <josh.drapergame@gmail.com>
2019-08-07 21:35:37 -0600

    MVP complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 import SavedList from './Movies/SavedList';
 
@@ -12,6 +15,9 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
+      <Route exact path='/' component={ MovieList } />
+      <Route path='/movies/:id' component={Movie} />
+
       <div>Replace this Div with your Routes</div>
     </div>
   );
